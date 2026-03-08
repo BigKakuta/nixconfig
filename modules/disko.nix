58eefa18101f032f7f9110c8d326a1e5d00087bc
Daniel Meyer <daniel@danielmeyer.io>
2026-03-08 19:18:56 +0100

    some experiment

---
+++ modules/disko.nix
@@ -0,0 +1,46 @@
+{ ... }: {
+  flake.diskoConfiguration.myMachineModule = {
+    disko.devices = {
+      disk.main = {
+        device = "/dev/nvme0n1";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              name = "boot";
+              size = "1M";
+              type = "EF02";
+            };
+            esp = {
+              name = "ESP";
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            nix = {
+              name = "nix";
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/nix";
+              };
+            };
+          };
+        };
+      };
+
+      nodev = {
+        "/" = {
+          fsType = "tmpfs";
+          mountOptions = [ "size=25%" "mode=755" ];
+        };
+      };
+    };
+  };
+}
