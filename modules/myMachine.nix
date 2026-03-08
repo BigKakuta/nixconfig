2fab85bc75532798779173bd5283192a31e62c25
Daniel Meyer <daniel@danielmeyer.io>
2026-03-08 15:18:45 +0100

    add installer

--- modules/myMachine.nix
+++ modules/myMachine.nix
@@ -1,14 +1,28 @@
 { inputs, self, ... }: {
 
-	flake.nixosConfigurations.myMachine = inputs.nixpkgs.lib.nixosSystem {
-		system = "x86_64-linux";
-		modules = [
-			self.nixosModules.myMachineModule
-			self.nixosModules.myFirstModule
-		];
-	};
+  flake.nixosConfigurations.myMachine = inputs.nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    modules = [
+      self.nixosModules.myMachineModule
+      self.nixosModules.myFirstModule
+    ];
+  };
 
-	flake.nixosModules.myMachineModule = { pkgs, ...}: {
-		boot.loader.grub.enable = true;
-	};
+  flake.nixosModules.myMachineModule = { ... }: {
+    boot.loader.grub.enable = true;
+
+    users.users.nixos = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBulB722y+drb1c3bRGJXkGIjVF/bWYfYd2NzXCo4Y5H danielmeyer@d5m4.com"
+      ];
+    };
+
+    security.sudo.wheelNeedsPassword = false;
+    nixpkgs.config.allowUnfree = true;
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+    services.openssh.enable = true;
+    system.stateVersion = "26.05";
+  };
 }
